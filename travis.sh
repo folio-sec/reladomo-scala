49374938054756f0fa67bed3e481339c4d02c596
matsu-chara <matsuy00@gmail.com>
2017-12-13 15:43:47 +0900

    support twitter_util 17.11, 17.12 & drop support 6.43,6.45,7.0
    Signed-off-by: matsu-chara <matsuy00@gmail.com>

--- travis.sh
+++ travis.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 if [[ ${TRAVIS_SCALA_VERSION} = "scripted-test" ]]; then
   sbt clean "project reladomoScalaCommon" +publishLocal \
+  && export TWITTER_UTIL_VERSION=17.12.0 \
+  && sbt clean "project reladomoScalaTwitterCommon" +publishLocal \
+  && export TWITTER_UTIL_VERSION=17.11.0 \
+  && sbt clean "project reladomoScalaTwitterCommon" +publishLocal \
   && export TWITTER_UTIL_VERSION=17.10.0 \
   && sbt clean "project reladomoScalaTwitterCommon" +publishLocal \
   && export TWITTER_UTIL_VERSION=7.1.0 \
-  && sbt clean "project reladomoScalaTwitterCommon" +publishLocal \
-  && export TWITTER_UTIL_VERSION=7.0.0 \
-  && sbt clean "project reladomoScalaTwitterCommon" +publishLocal \
-  && export TWITTER_UTIL_VERSION=6.45.0 \
-  && sbt clean "project reladomoScalaTwitterCommon" +publishLocal \
-  && export TWITTER_UTIL_VERSION=6.43.0 \
   && sbt clean "project reladomoScalaTwitterCommon" +publishLocal \
   && sbt clean \
       "project sbtReladomoPlugin" \
