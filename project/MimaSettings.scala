278b9cba792d1d21ee223b7f972162d63171f59f
ymatsubayashi <ymatsubayashi@folio-sec.com>
2018-03-07 12:02:05 +0900

    version 16.7.1-SNAPSHOT
    Signed-off-by: ymatsubayashi <ymatsubayashi@folio-sec.com>

--- project/MimaSettings.scala
+++ project/MimaSettings.scala
@@ -20,7 +20,7 @@ object MimaSettings {
     // because first release for this twitter util version, no binary compatbile check
     Set.empty
   } else {
-    Set(2).map(patch => s"16.6.$patch")
+    Set(2).map(patch => s"16.6.$patch") ++ Set(0).map(patch => s"16.7.$patch")
   }
 
   val mimaSettings = MimaPlugin.mimaDefaultSettings ++ Seq(
--- version.sbt
+++ version.sbt
@@ -1,4 +1,4 @@
 import Dependencies.reladomoBinVersion
 
 //scripted-plugin requires SNAPSHOT version since it publishes to local ivy in every test runs
-version in ThisBuild := s"${reladomoBinVersion}.0"
+version in ThisBuild := s"${reladomoBinVersion}.1-SNAPSHOT"
